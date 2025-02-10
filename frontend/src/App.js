1e36d1b42c876a287dcf63c4c80d1161ae6ea9f5
davidversegaming <69918596+davidversegaming@users.noreply.github.com>
2025-02-10 16:37:17 -0500

    Reset App.js and App.css to match Create React App template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
-      <p>Basic setup working</p>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
